1f37eb373ecd955b324ed3ac8b5570bd691f7b3d
comp8 <comp8@comp8_09.lab.unesc.net>
2017-03-31 21:06:06 -0300

    projeto inicial

--- CadastrarPaciente/src/login.java
+++ CadastrarPaciente/src/login.java
@@ -1,7 +1,7 @@
 
 import sun.awt.windows.WToolkit;
 
-public class login extends CadastroPaciente {
+public class login extends javax.swing.JFrame {
     
       public login() {
         initComponents();
